abd6caa3311569bc4b2b1066855c93ca4ea56991
Jose Carmona <josshue85@gmail.com>
2025-01-12 23:24:45 -0300

    Renderización de la simulación del carrito de compras OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import './App.css'
 import Cart from './components/Cart'
 import Footer from './components/Footer'
-import Home from './components/Home'
+// import Home from './components/Home'
 // import LoginPage from './components/LoginPage'
 import Navbar from './components/Navbar'
-import RegisterPage from './components/RegisterPage'
+// import RegisterPage from './components/RegisterPage'
 
 function App() {
   return (
     <div className="layout">
       {/* SOLO SE MOSTRARÁN LOS COMPONENTES NAVBAR, HOME Y FOOTER */}
       <Navbar />
-      <Home />
+      {/* <Home /> */}
       {/* <RegisterPage /> */}
       {/* <LoginPage /> */}
-      {/* <Cart /> */}
+      <Cart />
       <Footer />
     </div>
   )
